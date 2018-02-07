1ffcb1332e5651e75e140a2715a493fdb6952763
Howard Chen <H_Chen@alumni.baylor.edu>
2018-02-07 12:01:46 -0800

    Added basic state generation

--- main.py
+++ main.py
@@ -9,22 +9,25 @@ import httpcodes
 import string
 import random
 
-# USE THE GOOGLE FETCH LIBRARY TO MAKE HTTP REQUESTS TO GOOGLE OAUTH
+# For making HTTP requests from the GAE server.
+import urllib2
+import urlfetch
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         # Method to generate a random string is from
         # https://stackoverflow.com/questions/2257441/random
         #   -string-generation-with-upper-case-letters-and-digits-in-python
-        length = 10
+        length = 50
         state = ''.join(random.choice(
-            string.ascii + string.digits) for _ in length)
+            string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(length))
         # Ensure state can be verified when auth code comes back
         AccessHandler.state = state
 
         # From google documentation
         base_oauth_url = "https://accounts.google.com/o/oauth2/v2/auth?"
         scope = "email"
+        oauth_url = base_oauth_url
 
         template_values = {
             'url': oauth_url,
@@ -47,7 +50,7 @@ class AccessHandler(webapp2.RequestHandler):
 class AppHandler(webapp2.RequestHandler):
     state = "default"
     def get(self):
-
+        base = "https://localhost:8080"
 
 
 # [START app]
