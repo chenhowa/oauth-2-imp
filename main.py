6a58a470d774069efb78e46b74f5712a077c32f0
Howard Chen <chenhowa@oregonstate.edu>
2018-02-06 20:41:07 -0800

    Added basic research on OAuth

--- main.py
+++ main.py
@@ -5,24 +5,55 @@ import webapp2
 import json
 import httpcodes
 
-# [START main_page]
+# For generating random state variable.
+import string
+import random
+
+# USE THE GOOGLE FETCH LIBRARY TO MAKE HTTP REQUESTS TO GOOGLE OAUTH
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
+        # Method to generate a random string is from
+        # https://stackoverflow.com/questions/2257441/random
+        #   -string-generation-with-upper-case-letters-and-digits-in-python
+        length = 10
+        state = ''.join(random.choice(
+            string.ascii + string.digits) for _ in length)
+        # Ensure state can be verified when auth code comes back
+        AccessHandler.state = state
+
+        # From google documentation
+        base_oauth_url = "https://accounts.google.com/o/oauth2/v2/auth?"
+        scope = "email"
+
         template_values = {
-                
+            'url': oauth_url,
+            'state': state
         }
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
         httpcodes.write_created(self)
-# [END main_page]
+
+# Handles exchanging the authorization code for an access token
+class AccessHandler(webapp2.RequestHandler):
+    state = "default"
+    def get(self):
+        # Confirm that the state is correct.
+
+        # Swap the auth token for an access token
+        base = "https://www.googleapis.com/oauth2/v4/token"
+            
+
+class AppHandler(webapp2.RequestHandler):
+    state = "default"
+    def get(self):
+
+
 
 # [START app]
-# Add the patch method to the allowed HTTP verbs
-allowed_methods = webapp2.WSGIApplication.allowed_methods
-new_allowed_methods = allowed_methods.union(('PATCH',))
-webapp2.WSGIApplication.allowed_methods = new_allowed_methods
-
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/authorized', AppHandler),
+    ('/partial', AccessHandler),
 ], debug=True)
 # [END app]
